0dae54b57304a52ce460d28423d9fd42c8af8aa9
Dmitriy Mukhammadiev <metallistener@yandex.ru>
2026-08-20 10:17:52 +0500

    refactor: extract backend NODE_ENV checker into constants

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import healthRoutes from "./routes/health.route.js";
 import { app, server } from "./lib/socket.js";
+import { IS_PROD } from "./config/constants.js";
 
 // Load environment variables from .env file
 dotenv.config({
@@ -24,18 +25,18 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" 
-      ? ["http://localhost:8080", "http://localhost"] 
+    origin: IS_PROD
+      ? ["http://localhost:8080", "http://localhost"]
       : "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/health", healthRoutes);
 
-if (process.env.NODE_ENV === "production") {
+if (IS_PROD) {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
